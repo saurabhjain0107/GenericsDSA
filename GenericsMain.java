b4bc9cb40d7893a36929d91f82eefe2301630c29
saurabh jain <luckyjain0107@gmai.com>
2022-08-27 22:23:16 +0530

    Maximum Float Number

--- GenericsMain.java
+++ GenericsMain.java
@@ -3,8 +3,8 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class GenericsMain {
-    public int findMax(Integer n1,Integer n2,Integer n3){
-        Integer max = n1;
+    public float findMax(Float n1,Float n2,Float n3){
+        Float max = n1;
         if(n2.compareTo(max)>0) max = n2;
         if(n3.compareTo(max)>0) max = n3;
         return max;
@@ -13,13 +13,13 @@ public class GenericsMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first Number :");
-        int num1 = sc.nextInt();
+        float num1 = sc.nextFloat();
         System.out.println("Enter Second Number :");
-        int num2 = sc.nextInt();
+        float num2 = sc.nextFloat();;
         System.out.println("Enter Third Number :");
-        int num3 = sc.nextInt();
+        float num3 = sc.nextFloat();
         GenericsMain obj = new GenericsMain();
-        int maximum = obj.findMax(num1,num2,num3);
+        float maximum = obj.findMax(num1,num2,num3);
         System.out.println("Maximum number " +maximum);
     }
 }
