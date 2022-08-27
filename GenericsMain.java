61e4e044af40ad9e1023986879cde8918192c2b3
saurabh jain <luckyjain0107@gmai.com>
2022-08-27 22:27:05 +0530

    Maximum String Number

--- GenericsMain.java
+++ GenericsMain.java
@@ -3,8 +3,8 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class GenericsMain {
-    public float findMax(Float n1,Float n2,Float n3){
-        Float max = n1;
+    public String findMax(String n1,String n2,String n3){
+        String max = n1;
         if(n2.compareTo(max)>0) max = n2;
         if(n3.compareTo(max)>0) max = n3;
         return max;
@@ -13,13 +13,13 @@ public class GenericsMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first Number :");
-        float num1 = sc.nextFloat();
+        String num1 = sc.next();
         System.out.println("Enter Second Number :");
-        float num2 = sc.nextFloat();;
+        String num2 = sc.next();;
         System.out.println("Enter Third Number :");
-        float num3 = sc.nextFloat();
+        String num3 = sc.next();
         GenericsMain obj = new GenericsMain();
-        float maximum = obj.findMax(num1,num2,num3);
+        String maximum = obj.findMax(num1,num2,num3);
         System.out.println("Maximum number " +maximum);
     }
 }
